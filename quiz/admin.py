9aa2589c8c1e3aef6821d7ca8d4a1d854c61b026
Danush369 <mrcoder003@gmail.com>
2023-02-02 20:27:48 +0530

    1-only db structure created

---
+++ quiz/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from quiz.models import *
+# Register your models here.
+admin.site.register(qanda)
+admin.site.register(participantDetails)
+admin.site.register(response)
